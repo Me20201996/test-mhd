f8bc9e0ce5486b43c78dbaaf62aa2d35cb2a3a8b
Me20201996 <861777967@qq.com>
2020-07-25 17:09:56 +0800

    路由基础搭建

---
+++ src/views/SearchResult/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="page-search-result">
+    <h1>搜索结果页面</h1>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'SearchResult'
+
+}
+</script>
---
+++ src/views/Vip/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="page-vip">
+    <h1>vip专区</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Vip'
+
+}
+</script>
